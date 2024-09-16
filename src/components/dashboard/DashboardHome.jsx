c85f2e715d6cf7686ac92afd9ffddfb62de0d4be
EmmyTech50 <emmagospelnews@gmail.com>
2024-09-16 03:06:37 -0700

    i made changes to the profile icon name

--- src/components/dashboard/DashboardHome.jsx
+++ src/components/dashboard/DashboardHome.jsx
@@ -31,7 +31,7 @@ import Logo from '../../assets/neco_logo.svg';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import Dashboard from '../../assets/dashboard.svg';
 import MyT from '../../assets/mytoken.svg';
-import Profile from '../../assets/Profile.svg';
+import Profile from '../../assets/profile.svg';
 import CheckR from '../../assets/checkresult.svg';
 import ResultH from '../../assets/resulthistory.svg';
 import Transaction from '../../assets/transaction.svg';
