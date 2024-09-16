8f4d1ff18df7bae50da883043a70e59f99358ff6
EmmyTech50 <emmagospelnews@gmail.com>
2024-09-16 06:09:58 -0700

    i made changes in the vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://beautifulcolourslaundry.com.ng/",
+  // base: "https://beautifulcolourslaundry.com.ng/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom'],
+          // 'charting-lib': ['chart.js'],
+        },
+      },
+    },
+  },
 })
+
+
